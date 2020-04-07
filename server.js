5be692eef25e1dd386deabc7f12b7ad2103f2185
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-07 13:04:34 -0500

    fixed pipe

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const cors = require('cors');
 
 // Application Setup
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
